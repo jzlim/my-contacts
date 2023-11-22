06736a40dc88c14f299b8503e43232fd50b4a525
Jz.L <ljzhe96@gmail.com>
2023-11-22 19:37:46 +0800

    feat: setup contact list item component

--- src/ContactList/ContactListItem/ContactListItem.js
+++ src/ContactList/ContactListItem/ContactListItem.js
@@ -1,17 +1,17 @@
-function ContactListItem() {
+function ContactListItem({ profilePic, name, species, contactListClick }) {
   return (
-    <div className="flex flex-row items-center p-6 w-full gap-4 hover:cursor-pointer hover:bg-base-300">
+    <div
+      className="flex flex-row items-center p-6 w-full gap-4 hover:cursor-pointer hover:bg-base-300"
+      onClick={() => contactListClick()}
+    >
       <div className="avatar">
         <div className="w-12 lg:w-20 rounded-full">
-          <img
-            src="https://rickandmortyapi.com/api/character/avatar/566.jpeg"
-            alt="profile pic"
-          />
+          <img src={profilePic} alt="profile pic" />
         </div>
       </div>
       <div className="w-[70%] flex flex-col gap-1">
-        <span className="text-sm lg:text-md">Character Name</span>
-        <span className="text-sm lg:text-md">Species</span>
+        <span className="text-sm lg:text-md">{name}</span>
+        <span className="text-sm lg:text-md">{species}</span>
       </div>
     </div>
   );
