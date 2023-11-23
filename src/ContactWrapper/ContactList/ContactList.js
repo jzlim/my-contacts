0e3167836a3bfbdf077474adcb2e5799cbf66f45
Jz.L <ljzhe96@gmail.com>
2023-11-24 00:35:12 +0800

    feat: infinite scroll in contact list

--- src/ContactWrapper/ContactList/ContactList.js
+++ src/ContactWrapper/ContactList/ContactList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getDataFromApi } from "../../Api";
 import ContactListItem from "./ContactListItem/ContactListItem";
@@ -6,12 +6,22 @@ import Filter from "./Filter/Filter";
 
 function ContactList() {
   const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [maxNumberOfPage, setMaxNumberOfPage] = useState(1);
+  const sentinelRef = useRef(null);
+  const scrollContainerRef = useRef(null);
+  const [pageObj, setPageObj] = useState({
+    currentPage: 0,
+    nextPage: 1,
+    maxPage: undefined,
+  });
+  const [filterObject, setFilterObject] = useState({
+    name: "",
+    gender: "",
+    status: "",
+  });
+  const [isFilter, setIsFilter] = useState(false);
   const [items, setItems] = useState([]);
-  const [filterName, setFilterName] = useState("");
-  const [filterGender, setFilterGender] = useState("");
-  const [filterStatus, setFilterStatus] = useState("");
+  const [intersectionHandled, setIntersectionHandled] = useState(false);
+  const [isIntersected, setIsIntersected] = useState(false);
 
   const generateURLWithPayloads = (
     page,
@@ -33,90 +43,132 @@ function ContactList() {
     return url;
   };
 
-  const fetchData = (
-    page = 1,
-    isInitialFilter = false,
-    name = undefined,
-    gender = undefined,
-    status = undefined
-  ) => {
-    const url = generateURLWithPayloads(page, name, gender, status);
-    const fetchRequest = getDataFromApi(url);
-    fetchRequest.then((result) => {
-      if (result.results) {
-        setItems((prevItems) => {
-          if (isInitialFilter) {
-            return result.results;
-          } else {
-            return [...prevItems, ...result.results];
+  const fetchData = async (resetPagination = false) => {
+    if (!pageObj.nextPage && !resetPagination) {
+      return;
+    }
+    const url = generateURLWithPayloads(
+      resetPagination ? 1 : pageObj.nextPage,
+      filterObject.name,
+      filterObject.gender,
+      filterObject.status
+    );
+    const requestResult = await getDataFromApi(url);
+    if (requestResult) {
+      if (requestResult.results) {
+        setItems((currentItems) => {
+          if (resetPagination) {
+            return [...requestResult.results];
           }
+          return [...currentItems, ...requestResult.results];
         });
       } else {
         setItems([]);
       }
-      if (result.info) {
-        setMaxNumberOfPage(result.info.pages);
+      if (requestResult.info) {
+        setPageObj((obj) => {
+          const info = requestResult.info;
+          const currentPage = obj.currentPage + 1;
+          const nextPage = currentPage + 1;
+          return {
+            currentPage,
+            nextPage: info.next != null ? nextPage : undefined,
+            maxPage: requestResult.info.pages,
+          };
+        });
       }
+    }
+  };
+
+  const applyFilter = (filterType, value) => {
+    // scroll to most top to prevent the Intersection triggered when it's only performing filtering action
+    if (scrollContainerRef && scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTop = 0;
+    }
+
+    setFilterObject((obj) => {
+      let newFilterObject = { ...obj };
+      switch (filterType) {
+        case "name":
+          newFilterObject.name = value;
+          break;
+        case "gender":
+          newFilterObject.gender = value;
+          break;
+        case "status":
+          newFilterObject.status = value;
+          break;
+        default:
+          newFilterObject.name = ""; // requirement asked to clear only `status` and `gender` values
+          newFilterObject.gender = "";
+          newFilterObject.status = "";
+          break;
+      }
+      return newFilterObject;
     });
-  };
 
-  const loadMore = () => {
-    if (currentPage + 1 <= maxNumberOfPage) {
-      fetchData(currentPage + 1, false, filterName, filterGender, filterStatus);
-      setCurrentPage((value) => (value += 1));
-    }
-  };
+    setPageObj(() => {
+      return { currentPage: 0, nextPage: 1, maxPage: undefined };
+    });
 
-  const updateFilter = (filterType, value) => {
-    switch (filterType) {
-      case "name":
-        setFilterName(value);
-        fetchData(1, true, value, filterGender, filterStatus);
-        resetCurrentPage();
-        break;
-      case "gender":
-        setFilterGender(value);
-        fetchData(1, true, filterName, value, filterStatus);
-        resetCurrentPage();
-        break;
-      case "status":
-        setFilterStatus(value);
-        fetchData(1, true, filterName, filterGender, value);
-        resetCurrentPage();
-        break;
-      default:
-        // setFilterName(""); // requirement asked to clear only `status` and `gender` values
-        setFilterGender("");
-        setFilterStatus("");
-        fetchData(1, true, filterName);
-        resetCurrentPage();
-        break;
-    }
-  };
-
-  const resetCurrentPage = () => setCurrentPage(1);
-
-  const handleContactClick = (id) => {
-    navigate(`/contact/${id}`);
+    setIsFilter(() => true);
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (isFilter) {
+      fetchData(true);
+      setIsFilter(() => false);
+    } else if (isIntersected) {
+      fetchData();
+      setIsIntersected(() => false);
+    }
+  }, [filterObject, isFilter, isIntersected]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => intersectionObserverHandler(entries),
+      { threshold: 1 }
+    );
+
+    observer.observe(sentinelRef.current);
+
+    return () => {
+      if (sentinelRef.current) {
+        observer.unobserve(sentinelRef.current);
+      }
+    };
+  }, [intersectionHandled]);
+
+  const intersectionObserverHandler = (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      if (!intersectionHandled) {
+        setIsIntersected(() => true);
+        setIntersectionHandled(() => true); // set the flag to true to indicate the action has been taken
+      }
+    } else {
+      setIntersectionHandled(() => false); // reset the flag when the target is not intersecting
+    }
+  };
+
+  const handleContactClick = (id) => navigate(`/contact/${id}`);
 
   return (
     <div className="flex flex-col h-full">
       <div className="p-6">
         <span className="block text-xl lg:text-4xl mb-2 lg:mb-4">Contact</span>
         <Filter
-          searchKeywordChange={(value) => updateFilter("name", value)}
-          statusChange={(value) => updateFilter("status", value)}
-          genderChange={(value) => updateFilter("gender", value)}
-          clearFilter={() => updateFilter()}
+          searchKeywordChange={(value) => applyFilter("name", value)}
+          statusChange={(value) => applyFilter("status", value)}
+          genderChange={(value) => applyFilter("gender", value)}
+          clearFilter={() => applyFilter()}
         />
       </div>
       <div className="divider m-0"></div>
-      <div className="flex flex-col items-center overflow-y-auto">
+      <div
+        className="flex flex-col items-center overflow-y-auto"
+        ref={scrollContainerRef}
+      >
         {items.map((item, index) => (
           <ContactListItem
             key={index}
@@ -126,11 +178,9 @@ function ContactList() {
             contactListClick={() => handleContactClick(item.id)}
           />
         ))}
-        {currentPage + 1 <= maxNumberOfPage && (
-          <button className="link pb-4" onClick={loadMore}>
-            Load More
-          </button>
-        )}
+        <div ref={sentinelRef}></div>
+        {/* Empty <span> block is necessary here, otherwise the sentinelRef observer won't be triggered. */}
+        <span className="inline-block min-h-[.25rem] w-full"></span>
       </div>
     </div>
   );
