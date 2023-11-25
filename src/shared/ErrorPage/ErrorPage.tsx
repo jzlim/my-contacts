530b1403e9fd019265c925f9b8b19bce9e1fee21
Jz.L <ljzhe96@gmail.com>
2023-11-25 19:19:58 +0800

    fix: rectified import folder case-insensitve issue in git

---
+++ src/shared/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,21 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center w-full">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>
+          {(error as Error)?.message ||
+            (error as { statusText?: string })?.statusText}
+        </i>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
